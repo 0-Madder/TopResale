b01d354c4e05e02281c104c53cc1b8fbdf871a2c
madder <pablomartinezuni@gmail.com>
2023-04-25 13:30:33 +0200

    Pantalla de carga implementada

--- app/src/main/java/com/example/topresale/PantallaDeCargaActivity.java
+++ app/src/main/java/com/example/topresale/PantallaDeCargaActivity.java
@@ -2,12 +2,16 @@ package com.example.topresale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.WindowManager;
 
 
 public class PantallaDeCargaActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = LogInActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +19,17 @@ public class PantallaDeCargaActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_pantalla_de_carga);
 
+        new CountDownTimer(5000, 1000) {
+            public void onFinish() {
+                Log.d(LOG_TAG, "button_clicked");
+                Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+                startActivity(intent);
+            }
 
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
 
 
 
