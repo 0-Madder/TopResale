3c6fbe2dc5fb3defdcb6bc28475917591517c557
madder <pablomartinezuni@gmail.com>
2023-04-25 11:52:47 +0200

    Esconder las status bar

--- app/src/main/java/com/example/topresale/RegisterActivity.java
+++ app/src/main/java/com/example/topresale/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
     }
 
