5e50ba411b631858008a2567de3e834681422f2f
AlbertDigon <albertdigonq@gmail.com>
2023-04-25 13:25:30 +0200

    LOG IN MAIN

---
+++ app/src/main/java/com/example/topresale/LogInActivity.java
@@ -0,0 +1,40 @@
+package com.example.topresale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = LogInActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void iniciarSesion(View view) {
+        Log.d(LOG_TAG, "button_clicked");
+        Intent intent = new Intent(this, PantallaDeCargaActivity.class);
+        startActivity(intent);
+
+        //Caso en que el usuario no exista (necesitamos base de datos)
+
+        //Caso en que la contraseña no sea correcta (necesitamos la base de datos)
+    }
+
+    //Redirecciona al usuario a la página de registro
+    public void registrarse(View view) {
+        Log.d(LOG_TAG, "button_clicked");
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+}
