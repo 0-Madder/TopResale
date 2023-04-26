a1474b1af4b3359b4459f0a1c52687a92ea20410
Marc Cabrera <marcabrera.mcm@gmail.com>
2023-04-26 12:02:16 +0200

    b

--- app/src/main/java/com/example/topresale/model/UserManager.java
+++ app/src/main/java/com/example/topresale/model/UserManager.java
@@ -2,20 +2,43 @@ package com.example.topresale.model;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
 public class UserManager {
 
-    private FirebaseAuth mAuth;
-    private User user;
 
-    public void registrarUsuario(String nombre, String nombreUsuario){
+    private FirebaseAuth mAuth;
+    private ArrayList<User> llistaUsuaris;
+
+    public void registrarUsuario(String nomComplet, String correo, String nomUser, String pswd){
+        User user1 = new User(nomComplet,correo,nomUser,pswd);
 
     }
 
     public void inscriureUsuari(String textCorreo, String textPswd){
         mAuth.createUserWithEmailAndPassword(textCorreo, textPswd);
     }
-    public boolean usuariExistent (User user){
+
+    public User findUsuaricorreu (String correu) throws Exception{
+        for(User u : llistaUsuaris){
+            if(u.getCorreo().equalsIgnoreCase(correu)){
+                return u;
+            }
+
+        }
+        throw new Exception("Usuari per nom no trobat");
 
     }
+    public User findUsuarinom (String usuari) throws Exception{
+        for(User u : llistaUsuaris){
+            if(u.getNomUser().equalsIgnoreCase(usuari)){
+                return u;
+            }
+
+        }
+        throw new Exception("Usuari per correu no trobat");
+
+    }
+
 
 }
