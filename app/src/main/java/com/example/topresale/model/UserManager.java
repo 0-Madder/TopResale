208826b0ff7343e9e11136fc358dcec8bd11d96c
AlbertDigon <albertdigonq@gmail.com>
2023-04-26 12:46:00 +0200

    User

--- app/src/main/java/com/example/topresale/model/UserManager.java
+++ app/src/main/java/com/example/topresale/model/UserManager.java
@@ -1,10 +1,20 @@
 package com.example.topresale.model;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class UserManager {
+
+    private FirebaseAuth mAuth;
+    private User user;
 
     public void registrarUsuario(String nombre, String nombreUsuario){
 
+    }
 
+    public void inscriureUsuari(String textCorreo, String textPswd){
+        mAuth.createUserWithEmailAndPassword(textCorreo, textPswd);
+    }
+    public boolean usuariExistent (User user){
 
     }
 
